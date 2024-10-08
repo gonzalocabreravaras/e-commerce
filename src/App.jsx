52fd50885d1430dd44839e9c5a91c667a8cfe6c5
Gonzalo Cabrera <168502931+gonzalocabreravaras@users.noreply.github.com>
2024-10-08 20:19:16 -0300

    Primer push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Componentes/Header/header';
+import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer';
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header></Header>
+      <ItemListContainer saludo="¡Bienvenido a nuestra tienda oficial! ⚽"></ItemListContainer>
+    </div>
+    
+    
+  )
+}
+
+export default App
